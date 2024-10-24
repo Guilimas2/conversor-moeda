b0286a28951c77f00218bbdfb0489532d9a7db42
GUILHERME SERAFIM DE LIMA <guilherme.serafim@alunoescolassociais.org.br>
2024-10-24 11:52:45 -0300

    inicie o javascript

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('currency-form').addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    // obter valores de entrada informados pelo usuario
+    const valor = parseFloat(document.getElementById('valor'));
+});
