63e50636df246b6e24dcf36d2ba68c81615da7e8
GUILHERME SERAFIM DE LIMA <guilherme.serafim@alunoescolassociais.org.br>
2024-10-25 07:53:38 -0300

    voltamos para trablhos

--- main.js
+++ main.js
@@ -3,4 +3,14 @@ document.getElementById('currency-form').addEventListener('submit', function(eve
     
     // obter valores de entrada informados pelo usuario
     const valor = parseFloat(document.getElementById('valor'));
+    const deMoeda = document.getElementById('deMoeda').value;
+    const paraMoeda = document.getElementById('paraMoeda').value;
+
+    // definir taxas de cambio fixas
+    const exchangeRates = {
+        USD: {BRL: 5.70, EUR: 0.93},
+        BRL: {USD: 0.18, EUR: 0.16},
+        EUR: {USD: 1.08, BRL: 6.16},
+        
+    };
 });
